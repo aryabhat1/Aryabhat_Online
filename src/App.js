5d12404465b27cdaaff09dfa497ed1572314bad5
Aryabhat1 <sushant.pru@gmail.com>
2021-11-14 16:02:54 +0530

    Portfolio setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>Hello World!</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
