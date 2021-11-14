ee44d499262fcc55d454b56ddb80eeacfc02bd4f
Aryabhat1 <sushant.pru@gmail.com>
2021-11-14 23:26:52 +0530

    header commited

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css'
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import { Particles } from "tsparticles/Core/Particles";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Navbar />
+        <>    
+          <Navbar />
+          <Header />
+        </>
         <h3>Hello World!</h3>
       </header>
     </div>
